120f08a8ad770da6dd792b0b43452539b91e83ca
Gavin Whelan <gwhelan@launchdarkly.com>
2020-07-14 12:12:06 -0700

    Make main product dynamic.

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "LDSwiftEventSource",
+            type: .dynamic,
             targets: ["LDSwiftEventSource"]),
     ],
     dependencies: [],
