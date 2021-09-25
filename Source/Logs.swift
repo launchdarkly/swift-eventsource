efae19097b77c69f584e031d5c05be9f2f19e593
Gavin Whelan <gwhelan@launchdarkly.com>
2021-09-25 10:37:34 -0500

    Add logging wrapper to centralize OS checks.

---
+++ Source/Logs.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+#if !os(Linux)
+import os.log
+#endif
+
+class Logs {
+    enum Level {
+        case debug, info, warn, error
+
+        #if !os(Linux)
+        private static let osLogTypes = [ Level.debug: OSLogType.debug,
+                                          Level.info: OSLogType.info,
+                                          Level.warn: OSLogType.default,
+                                          Level.error: OSLogType.error]
+        var osLogType: OSLogType { Level.osLogTypes[self]! }
+        #endif
+    }
+
+    private let logger: OSLog = OSLog(subsystem: "com.launchdarkly.swift-eventsource", category: "LDEventSource")
+
+    func log(_ level: Level, _ staticMsg: StaticString, _ args: CVarArg...) {
+        #if !os(Linux)
+        os_log(staticMsg, log: logger, type: level.osLogType, args)
+        #endif
+    }
+}
