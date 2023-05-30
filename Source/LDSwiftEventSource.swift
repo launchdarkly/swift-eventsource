3f2130fb6da9f6dea747e47f9624d45296b55780
Matthew Keeler <mkeeler@launchdarkly.com>
2023-05-30 13:48:12 -0400

    Not supported on Linux

--- Source/LDSwiftEventSource.swift
+++ Source/LDSwiftEventSource.swift
@@ -86,11 +86,13 @@ public class EventSource {
                 sessionConfig.httpAdditionalHeaders = ["Accept": "text/event-stream", "Cache-Control": "no-cache"]
                 sessionConfig.timeoutIntervalForRequest = idleTimeout
 
+                #if !os(Linux)
                 if #available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *) {
                     sessionConfig.tlsMinimumSupportedProtocolVersion = .TLSv12
                 } else {
                     sessionConfig.tlsMinimumSupportedProtocol = .tlsProtocol12
                 }
+                #endif
                 return sessionConfig
             }
             set {
